037d2ebe3fd669831bd01a1339ca826100946628
Jaecobd1 <jaecob.d1@gmail.com>
2022-07-13 20:50:31 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    price = db.Column(db.Integer, default=0)
+    
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__== "__main__":
+    app.run(debug=True)
+
